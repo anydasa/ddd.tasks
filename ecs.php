773cb521eba68a5a32ed80cb551bae68d4c28c8b
Artem Sykchin <artem.sykchin@ys.bet>
2021-02-01 20:51:10 +0200

    Change linter tool php-fixer to easy-coding-standard

---
+++ ecs.php
@@ -0,0 +1,18 @@
+<?php
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->set(ArraySyntaxFixer::class)
+        ->call('configure', [[
+            'syntax' => 'short',
+        ]]);
+
+    // B. full sets
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::SETS, [SetList::CLEAN_CODE, SetList::PSR_12, SetList::SYMFONY]);
+};
