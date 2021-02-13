23bd17e10138eba089596b5c061abbd2cdd6a336
Artem Sykchin <artem.sykchin@ys.bet>
2021-02-13 17:14:37 +0200

    move Dataprovider ro CreateTaskCommandTest

--- tests/Application/Command/Task/CreateTask/CreateTaskCommandTest.php
+++ tests/Application/Command/Task/CreateTask/CreateTaskCommandTest.php
@@ -3,8 +3,10 @@
 namespace Test\Application\Command\Task\CreateTask;
 
 use App\Application\Command\Task\CreateTask\CreateTaskCommand;
+use App\Domain\Task\ValueObject\Priority\PriorityTypes;
 use League\Tactician\Bundle\Middleware\InvalidCommandException;
 use League\Tactician\CommandBus;
+use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class CreateTaskCommandTest extends KernelTestCase
@@ -20,7 +22,7 @@ class CreateTaskCommandTest extends KernelTestCase
 
     /**
      * @group integration
-     * @dataProvider \Test\Application\Command\Task\CreateTask\DataProvider::getValidCommandData
+     * @dataProvider getValidCommandData
      */
     public function testSuccess(string $id, string $label, string $description, string $priority, string $date)
     {
@@ -32,7 +34,7 @@ class CreateTaskCommandTest extends KernelTestCase
 
     /**
      * @group integration
-     * @dataProvider \Test\Application\Command\Task\CreateTask\DataProvider::getInvalidCommandData
+     * @dataProvider getInvalidCommandData
      */
     public function testException(string $id, string $label, string $description, string $priority, string $date)
     {
@@ -40,5 +42,25 @@ class CreateTaskCommandTest extends KernelTestCase
 
         $command = new CreateTaskCommand($id, $label, $description, $priority, $date);
         $this->commandBus->handle($command);
+    }
+
+    public function getValidCommandData(): array
+    {
+        return [
+            [Uuid::uuid4()->toString(), 'Label1', 'Description1', PriorityTypes::CODE_LOW, '2020-12-01'],
+            [Uuid::uuid4()->toString(), 'Label2', 'Description2', PriorityTypes::CODE_MEDIUM, '2021-12-01'],
+            [Uuid::uuid4()->toString(), 'Label3', 'Description3', PriorityTypes::CODE_HIGH, '2021-12-01'],
+        ];
+    }
+
+    public function getInvalidCommandData(): array
+    {
+        return [
+            [Uuid::uuid4()->toString(), 'Label', 'Description', PriorityTypes::CODE_LOW, '2020-12-0'],
+            [Uuid::uuid4()->toString(), 'Label', 'Description', 'Wrong priority', '2021-12-01'],
+            [Uuid::uuid4()->toString(), 'Label', '', PriorityTypes::CODE_HIGH, '2021-12-01'],
+            [Uuid::uuid4()->toString(), '', 'Description', PriorityTypes::CODE_HIGH, '2021-12-01'],
+            ['', 'Label', 'Description', PriorityTypes::CODE_HIGH, '2021-12-01'],
+        ];
     }
 }
