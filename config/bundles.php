96ea220d41e0041af617eebeeb329d4abfc5791e
Artem Sykchin <artem.sykchin@ys.bet>
2021-02-26 19:47:38 +0200

    Remove TacticianDomainEventBundle

--- config/bundles.php
+++ config/bundles.php
@@ -5,7 +5,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
-    BornFree\TacticianDomainEventBundle\TacticianDomainEventBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
--- src/Domain/Task/Task.php
+++ src/Domain/Task/Task.php
@@ -4,20 +4,15 @@ declare(strict_types=1);
 
 namespace App\Domain\Task;
 
-use App\Domain\Task\Event\TaskWasCreated;
 use App\Domain\Task\ValueObject\Description;
 use App\Domain\Task\ValueObject\DueDate;
 use App\Domain\Task\ValueObject\Label;
 use App\Domain\Task\ValueObject\Priority;
 use App\Domain\Task\ValueObject\Status;
 use App\Domain\Task\ValueObject\TaskId;
-use BornFree\TacticianDomainEvent\Recorder\ContainsRecordedEvents;
-use BornFree\TacticianDomainEvent\Recorder\EventRecorderCapabilities;
 
-final class Task implements ContainsRecordedEvents
+final class Task
 {
-    use EventRecorderCapabilities;
-
     private TaskId $id;
 
     private Label $label;
@@ -43,12 +38,6 @@ final class Task implements ContainsRecordedEvents
         $self->status = $status;
         $self->priority = $priority;
         $self->dueDate = $dueDate;
-
-        $self->record(new TaskWasCreated(
-            (string) $id,
-            (string) $label,
-            (string) $description
-        ));
 
         return $self;
     }
